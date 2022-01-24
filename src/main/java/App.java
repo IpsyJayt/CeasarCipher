38ae15d7038082f911cdd620da2fde31393a3d7d
IpsyJayt <ipsyjayt@gmail.com>
2022-01-24 15:08:34 +0300

    put in a while loop to run the user options, add code to select the users first option: encode,decode,exit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,14 @@ public class App{
     public static void main (String[] args) {
         Scanner userInput = new Scanner(System.in);
         boolean selectionsLoop = true;
+
+        System.out.println("Welcome to CAESAR CIPHER APPLICATION");
+        while(selectionsLoop){
+            System.out.println("..................................................");
+            System.out.println("Select what you'd like to do(Enter the number of your option): \n" + "1:Encode message \n" + "2:Decrypt message \n" + "3:Exit");
+            int userSelection = userInput.nextInt();
+            userInput.nextLine(); //THE INPUT TO BE USED BY nextInt();
+
+        }
     }
 }
