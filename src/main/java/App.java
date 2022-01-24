2556fb1b6782fe781798c871700033fa42c12448
IpsyJayt <ipsyjayt@gmail.com>
2022-01-24 14:58:22 +0300

    create the App class, import scanner to taker user input

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class App{
+    public static void main (String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        boolean selectionsLoop = true;
+    }
+}
