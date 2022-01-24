3ba1afe12c814ed5fcb8489cdbb5813174c82cc6
IpsyJayt <ipsyjayt@gmail.com>
2022-01-24 11:17:24 +0300

    add the code to execute the behavior

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,14 @@
+public class Cipher {
+    private String inputText;
+    private String encryptedText;
+    private String decryptedText;
+
+    private String[] alphaBets = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+
+    public Cipher (String inputText){
+        this.inputText = inputText;
+        this.encryptedText = "";
+        this.decryptedText = "";
+    }
+}
